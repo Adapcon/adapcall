3729c86835fbb73b5741443662f8916d92914e13
Germano Gascho <germano@adapcon.com.br>
2020-01-25 09:56:50 -0300

    lista de ligação e criar ligação

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,60 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+		<table>
+			<tr>
+				<th> ID </th>
+				<th> Solicitante </th>
+				<th> Solicitado </th>
+			</tr>
+			<tr v-for="ligacao in ligacoes" :key="ligacao.id">
+				<td> {{ ligacao.id }} </td>
+				<td> {{ ligacao.solicitante }} </td>
+				<td> {{ ligacao.solicitado }}
+					<span v-if="ligacao.urgente"> URGENTE!!! </span>
+					<span v-else> (de boas) </span>
+				</td>
+			</tr>
+		</table>
+		<input
+			type="button"
+			@click="criarLigacao"
+			value="Criar Ligação"
+		>
   </div>
 </template>
 
+<script>
+export default {
+	data() {
+		return {
+			ligacoes: [
+				{
+					id: 2424,
+					solicitante: "Germano",
+					solicitado: "Luís TI Colcci",
+					urgente: false,
+				},
+				{
+					id: 2425,
+					solicitante: "Márcio",
+					solicitado: "Fernando TI Colcci",
+					urgente: true,
+				},
+			],
+		};
+	},
+	methods: {
+		criarLigacao() {
+			this.ligacoes.push({
+				id: 2426,
+				solicitante: "Sérgio",
+				solicitado: "Luís TI Colcci",
+				urgente: false,
+			})
+		}
+	}
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
